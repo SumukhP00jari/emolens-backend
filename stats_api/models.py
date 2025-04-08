1a6c1551e13e8c16c68c8b6222d6ad6efe031f9a
SumukhP00jari <sumukhpoojari0028@gmail.com>
2025-04-08 17:54:24 +1000

    Updating db model.py

--- stats_api/models.py
+++ stats_api/models.py
@@ -7,10 +7,12 @@ class ADHD(models.Model):
 
     class Meta:
         db_table = 'adhd'
+        managed = False  # <== Important: Do not try to manage this table (since it's from an existing DB)
 
 class Neurodivergent(models.Model):
-    year = models.CharField(max_length=10)
+    year = models.CharField(max_length=20, primary_key=True)  # Assuming year is unique
     rate_million = models.FloatField()
 
     class Meta:
         db_table = 'neurodivergent'
+        managed = False
