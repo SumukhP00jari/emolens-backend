204ac344765f061de9bb54b737e75c7ab468ca6a
SumukhP00jari <sumukhpoojari0028@gmail.com>
2025-04-24 04:40:38 +1000

    Fixing errors in the code

--- stats_api/views.py
+++ stats_api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db import models
 
 from django.db import connection
 from rest_framework.views import APIView
@@ -25,61 +26,80 @@ class NeurodivergentStatsAPIView(APIView):
         serializer = NeurodivergentSerializer(data, many=True)
         return Response(serializer.data)
 
-class LearningHubAPIView(APIView):
-    def get(self, request):
-        data = LearningHub.objects.all()
-        serializer = LearningHubSerializer(data, many=True)
-        return Response(serializer.data)
+class LearningHub(models.Model):
+    question = models.TextField(primary_key=True)
+    content = models.TextField()
+    data_insight = models.TextField(null=True, blank=True)
 
+    class Meta:
+        db_table = 'learning_hub'
+        managed = False
 
-class ADHDTreatmentAPIView(APIView):
-    def get(self, request):
-        data = ADHDTreatment.objects.all()
-        serializer = ADHDTreatmentSerializer(data, many=True)
-        return Response(serializer.data)
+class ADHDTreatment(models.Model):
+    treatment_type = models.CharField(max_length=50, primary_key=True)
+    age_group = models.CharField(max_length=50)
+    percentage = models.FloatField()
 
+    class Meta:
+        db_table = 'adhd_treatment'
+        managed = False
 
-class ADHDDisorderAPIView(APIView):
-    def get(self, request):
-        data = ADHDDisorder.objects.all()
-        serializer = ADHDDisorderSerializer(data, many=True)
-        return Response(serializer.data)
+class ADHDPrevalenceYear(models.Model):
+    year = models.IntegerField()
+    sex = models.CharField(max_length=10)
+    adhd_estimate = models.IntegerField()
 
+    class Meta:
+        db_table = 'adhd_prevalence_year'
+        managed = False
+        unique_together = ('year', 'sex')
 
-class ADHDPrevalenceYearAPIView(APIView):
-    def get(self, request):
-        data = ADHDPrevalenceYear.objects.all()
-        serializer = ADHDPrevalenceYearSerializer(data, many=True)
-        return Response(serializer.data)
+class ADHDPrevalenceAge(models.Model):
+    sex = models.CharField(max_length=10)
+    age_group = models.CharField(max_length=20)
+    adhd_estimate = models.IntegerField()
 
+    class Meta:
+        db_table = 'adhd_prevalence_age'
+        managed = False
+        unique_together = ('sex', 'age_group')
 
-class ADHDPrevalenceAgeAPIView(APIView):
-    def get(self, request):
-        data = ADHDPrevalenceAge.objects.all()
-        serializer = ADHDPrevalenceAgeSerializer(data, many=True)
-        return Response(serializer.data)
+class ADHDPrescription(models.Model):
+    age_group = models.CharField(max_length=50)
+    year = models.CharField(max_length=20)
+    count = models.IntegerField()
 
+    class Meta:
+        db_table = 'adhd_prescription'
+        managed = False
+        unique_together = ('age_group', 'year')
 
-class ADHDPrescriptionAPIView(APIView):
-    def get(self, request):
-        data = ADHDPrescription.objects.all()
-        serializer = ADHDPrescriptionSerializer(data, many=True)
-        return Response(serializer.data)
+class ADHDDisorder(models.Model):
+    mental_disorder = models.CharField(max_length=50)
+    sex = models.CharField(max_length=10)
+    prevalence = models.FloatField()
 
+    class Meta:
+        db_table = 'adhd_disorder'
+        managed = False
+        unique_together = ('mental_disorder', 'sex')
 
-class ADHDConditionAPIView(APIView):
-    def get(self, request):
-        data = ADHDCondition.objects.all()
-        serializer = ADHDConditionSerializer(data, many=True)
-        return Response(serializer.data)
+class ADHDCondition(models.Model):
+    age_group = models.CharField(max_length=20)
+    conditions = models.CharField(max_length=100, primary_key=True)
+    percentage = models.FloatField()
 
+    class Meta:
+        db_table = 'adhd_condition'
+        managed = False
 
-class ADHDDayOffAPIView(APIView):
-    def get(self, request):
-        data = ADHDDayOff.objects.all()
-        serializer = ADHDDayOffSerializer(data, many=True)
-        return Response(serializer.data)
+class ADHDDayoff(models.Model):
+    disorder = models.CharField(max_length=100, primary_key=True)
+    average_days_absent = models.IntegerField()
 
+    class Meta:
+        db_table = 'adhd_dayoff'
+        managed = False
 
 class DBConnectionCheck(APIView):
     def get(self, request):
