a0499adfe488b3e8decf8bcf556f85958c341de8
SumukhP00jari <sumukhpoojari0028@gmail.com>
2025-04-25 09:04:42 +1000

    changed to this DEBUG = True

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-!&x4c%%qx3-*o3zso6==c26*wdi0k_uf-7#__4(c9w2un)&iak"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['api.kindlepeers.me', 'localhost', '127.0.0.1']
 
