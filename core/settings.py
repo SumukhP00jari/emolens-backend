761be123ae452d1d7ac3b8852753f7ba896a4d10
SumukhP00jari <sumukhpoojari0028@gmail.com>
2025-04-26 20:38:27 +1000

    localhost access to fronend change in code

--- core/settings.py
+++ core/settings.py
@@ -94,7 +94,11 @@ DATABASES = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "https://www.kindlepeers.me",  
+    "https://www.kindlepeers.me", 
+    "http://localhost:3000",        
+    "http://127.0.0.1:3000",          
+    "http://localhost:5173",          
+    "http://127.0.0.1:5173", 
 ]
 
 # Password validation
