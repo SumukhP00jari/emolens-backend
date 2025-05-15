3d85c517d99f0362e6a3df3290269635cc7d98d6
SumukhP00jari <sumukhpoojari0028@gmail.com>
2025-05-15 17:54:57 +1000

    error fixing for quiz db

--- emotion_quiz_api/models.py
+++ emotion_quiz_api/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
+
 class EmotionGuessing(models.Model):
-    question_id = models.IntegerField()
-    image_url = models.TextField()
-    answer_id = models.IntegerField()
-    answer_desc = models.CharField(max_length=100)
-    correct_answer_id = models.IntegerField()
+    question_id = models.IntegerField(primary_key=True)  
+    image_url = models.TextField(null=True)
+    answer_id = models.IntegerField(null=True)
+    answer_desc = models.CharField(max_length=100, null=True)
+    correct_answer_id = models.IntegerField(null=True)
 
     class Meta:
         managed = False
         db_table = 'emotion_guessing'
-        unique_together = (('question_id', 'answer_id'),)
