ef92f5ffc0c815e7e7e4dbced0ae0b79f738fd48
SumukhP00jari <sumukhpoojari0028@gmail.com>
2025-05-15 19:40:24 +1000

    using a composite key as primary key

--- emotion_quiz_api/models.py
+++ emotion_quiz_api/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
 
+
 class EmotionGuessing(models.Model):
-    id = models.AutoField(primary_key=True)  
     question_id = models.IntegerField()
     image_url = models.TextField(null=True)
-    answer_id = models.IntegerField(null=True)
+    answer_id = models.IntegerField()
     answer_desc = models.CharField(max_length=100, null=True)
-    correct_answer_id = models.IntegerField(null=True)
+    correct_answer_id = models.IntegerField()
 
     class Meta:
         managed = False
         db_table = 'emotion_guessing'
+        unique_together = (('question_id', 'answer_id'),)
