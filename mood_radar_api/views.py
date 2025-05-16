2b2ae872bd06ba93d2ca6e0af15baba5a92e8dec
SumukhP00jari <sumukhpoojari0028@gmail.com>
2025-05-16 14:30:03 +1000

    edited code for views.py

--- mood_radar_api/views.py
+++ mood_radar_api/views.py
@@ -1,44 +1,23 @@
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Input, SeparableConv2D, MaxPooling2D, Flatten, Dense
-
 import base64
 import io
 import numpy as np
-from django.conf import settings
 from pathlib import Path
 from PIL import Image
+from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.conf import settings
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import SeparableConv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
 
-model_path = Path(settings.BASE_DIR) / 'mood_radar_api' / 'emotion_model.h5'
 EMOTION_LABELS = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+model_path = Path(settings.BASE_DIR) / 'mood_radar_api' / 'emotion_model.h5'
 
-def build_model():
-    model = Sequential()
-    model.add(Input(shape=(48, 48, 1)))  
-    model.add(SeparableConv2D(32, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(SeparableConv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(SeparableConv2D(128, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Flatten())
-    model.add(Dense(64, activation='relu'))
-    model.add(Dense(len(EMOTION_LABELS), activation='softmax'))
-    return model
-
-model = build_model()
 try:
-    model.load_weights(model_path)
+    model = load_model(model_path, compile=False)
     model_loaded = True
 except Exception as e:
-    print(f" Error loading model: {e}")
+    print(f"Error loading model: {e}")
     model_loaded = False
 
 class MoodRadarAPIView(APIView):
@@ -52,10 +31,10 @@ class MoodRadarAPIView(APIView):
 
         try:
             image_data = base64.b64decode(base64_str)
-            image = Image.open(io.BytesIO(image_data)).convert("L")  
+            image = Image.open(io.BytesIO(image_data)).convert("L")
             image = image.resize((48, 48))
             image_array = img_to_array(image) / 255.0
-            image_array = np.expand_dims(image_array, axis=0)  
+            image_array = np.expand_dims(image_array, axis=0)
 
             predictions = model.predict(image_array)[0]
             top3_indices = predictions.argsort()[-3:][::-1]
@@ -64,6 +43,5 @@ class MoodRadarAPIView(APIView):
                 for i in top3_indices
             ]
             return Response({"predictions": results}, status=200)
-
         except Exception as e:
             return Response({"error": f"Error during prediction: {str(e)}"}, status=500)
